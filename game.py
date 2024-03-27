5add0cbf826c1e3a230fa343a5dafdb6bee94330
LaviniaIonescu <laviniaionescu9933@gmail.com>
2024-03-27 18:34:01 +0200

    finished developing change highscore function also adds date
    game now waits 2 seconds and allows player to exit

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import copy
+import datetime
 import json
 import random
 import time
@@ -6,8 +7,18 @@ import time
 POSSIBLE_ANSWERS = {0: 'a.', 1: 'b.', 2: 'c.', 3: 'd.'}
 
 
-def change_highscore():
-    pass
+def change_highscore(player_id: str, score: int, path: str = "users.json"):
+    try:
+        with open(path, "r+") as f:
+            players = json.loads(f.read())
+            players[player_id]['high_score'] = score
+            players[player_id]['date'] = datetime.datetime.now().strftime("%Y/%m/%d, %H:%M")
+            f.seek(0)
+            f.write(json.dumps(players, indent=4))
+    except Exception as e:
+        print(f"Failed to save the high score of {player_id}.\n Error is {e}.")
+    else:
+        print("Successfully saved the new high score.")
 
 
 def run_game(player: dict, questions_path: str = "questions.json") -> int:
@@ -21,7 +32,7 @@ def run_game(player: dict, questions_path: str = "questions.json") -> int:
     while copy_questions:
         question_object = random.choice(copy_questions)
 
-        print(question_object)
+        # print(question_object)
         print(question_object['question'])
         for index, answer in enumerate(question_object['answers']):
             print(f"{POSSIBLE_ANSWERS[index]} {answer}")
@@ -40,6 +51,6 @@ def run_game(player: dict, questions_path: str = "questions.json") -> int:
     print(f"You have answered to {score} questions correctly!")
 
     if score > player[list(player.keys())[0]]['high_score']:
-        change_highscore()
+        change_highscore(player_id=list(player.keys())[0], score= score)
 
     return 1
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # create a quiz game with admin and players.
 # a user has to login. if player then he can play, if admin, can add questions
 import json
+import time
+
 import users
 import game
 
@@ -14,6 +16,9 @@ if __name__ == '__main__':
     current_player = users.login()
 
     while True:
-
         print(f"Let's play, {list(current_player.keys())[0]}")
         game.run_game(current_player)
+        time.sleep(2)
+        user_pick = input("Do you want to play again? Y/N: ")
+        if user_pick.lower() == "n":
+            break
